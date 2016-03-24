f506d38d985a601b9c453c808e7355c138116a2a
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2016-03-24 17:41:09 +0000

    Modified

--- configurations.js
+++ configurations.js
@@ -8,26 +8,43 @@
 module.exports = function(resource) {
   console.log('Configurations called');
   var path = require('../libraries/path');
-  var paths = require('../paths/paths'); 
+  var paths = require('../paths/paths');
   var fs = require(path.join(paths.libraries, '/fs.js'));
   var uri = require(path.join(paths.libraries, '/uri.js'));
   var request = require(path.join(paths.libraries, '/request.js'));
   var _Configurations = {};
-  var URI = resource.URI;
+  // START - TEMPORARY ONLY !!!
+  if(!resource instanceof Array){
+	console.log('Configurations - resource provided is not an instance of Array');
+	resource = {};
+  }
+  // END - TEMPORARY ONLY !!!
+  // START - TEMPORARY ONLY !!!
+  if(resource.URI == null){
+	console.log('Configurations - no resource.URI provided');
+	//temporary fix:
+	var scheme = 'urn'; // e.g. urn, url
+    var namespaceIdentifier = 'uuid'; // e.g. uuid, http, https
+    var namespaceSpecificString = '6e8bc430-9c3a-11d9-9669-0800200c9a66'; // e.g. 6e8bc430-9c3a-11d9-9669-0800200c9a66, //example.org/foo?bar=baz
+	resource.URI = new URI(scheme+':'+namespaceIdentifier+':'+namespaceSpecificString);
+	console.log('Configurations - fixed URI: ', resource.URI); // states undefined ... WHY?
+  }
+  // END - TEMPORARY ONLY !!!
   var lookup = function(configurations, configuration, resource) {
 	console.log('Configurations.lookup called');
-	var scheme = URI.scheme(); // get scheme from URI e.g. 'urn' or 'url';
+	console.log('Configurations.lookup - resource: ', resource);
+	var scheme = resource.URI.scheme(); // get scheme from URI e.g. 'urn' or 'url';
 	console.log('Configurations - scheme: ', scheme);
-	var namespaceIdentifier = URI.heirpart().value.split(':')[0]; // get NID from URI e.g. 'uuid' or 'http'
+	var namespaceIdentifier = resource.URI.heirpart().value.split(':')[0]; // get NID from URI e.g. 'uuid' or 'http'
 	console.log('Configurations - namespaceIdentifier: ', namespaceIdentifier);
-	var namespaceSpecificString = URI.heirpart().value.split(':')[1]; //get NSS from URI e.g. '6e8bc430-9c3a-11d9-9669-0800200c9a66'
+	var namespaceSpecificString = resource.URI.heirpart().value.split(':')[1]; //get NSS from URI e.g. '6e8bc430-9c3a-11d9-9669-0800200c9a66'
 	console.log('Configurations - namespaceSpecificString: ', namespaceSpecificString);
 	switch(scheme) {
 	  case 'url:':
 	    // handle url, for remote files
 		switch(configuration) {
 		  case 'applications':
-			var url = path.resolve(URI.heirpart().value, configuration+'.js'); // e.g. 'http://www.whatever.com/configs/applications.js'
+			var url = path.resolve(resource.URI.heirpart().value, configuration+'.js'); // e.g. 'http://www.whatever.com/configs/applications.js'
 			request.get(url, function (error, response, body) {
 			  if (!error && response.statusCode == 200) {
 				configurations.applications = body(resource);
@@ -66,7 +83,7 @@ module.exports = function(resource) {
 		    });			
 			break;
 		  case 'servers':
-            var url = path.resolve(URI.heirpart().value, configuration+'.js'); // e.g. 'http://www.whatever.com/configs/servers.js'
+            var url = path.resolve(resource.URI.heirpart().value, configuration+'.js'); // e.g. 'http://www.whatever.com/configs/servers.js'
 			request.get(url, function (error, response, body) {
 			  if (!error && response.statusCode == 200) {		  
 			    configurations.servers = body(resource);
@@ -86,6 +103,24 @@ module.exports = function(resource) {
         // handle urn, for local files
 		var file = path.resolve(__dirname, namespaceSpecificString, configuration+'.js');
 		console.log('Configurations - File: ', file);
+		
+		//---- START TEST AREA
+		
+		// var dirname = __dirname;
+		// console.log('TEST AREA __dirname: ', dirname);
+		
+		// fs.openSync(file, 'rs', function(err, fd) {
+		  // if (err && err.code=='ENOENT') { 
+			// /* file doesn't exist */ 
+			// console.log('TESTAREA File does not exist: ', __dirname+'/6e8bc430-9c3a-11d9-9669-0800200c9a66/applications.js');
+		  // }
+          // else {
+		    // console.log('TESTAREA File does exist: ', __dirname+'/6e8bc430-9c3a-11d9-9669-0800200c9a66/applications.js');
+		  // }
+		// });
+		
+		//---- END TEST AREA
+		
 		fs.openSync(file,'rs',function(err,fd){
 		  console.log('fs.open called');
 		  if (err && err.code=='ENOENT') { 
@@ -125,7 +160,7 @@ module.exports = function(resource) {
   lookup(_Configurations, 'common', resource);
   lookup(_Configurations, 'databases', resource);
   lookup(_Configurations, 'servers', resource);
-	
+
   console.log('Returning Configurations');
   return _Configurations;
 }
