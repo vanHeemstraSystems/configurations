a056b1f691703842eab7cb3a66369183c2e668d6
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2016-04-24 21:56:29 +0100

    Modified

--- configuration/index.js
+++ configuration/index.js
@@ -1,7 +1,7 @@
 /*
  * index.js
  */
-var Configuration6e8bc430-9c3a-11d9-9669-0800200c9a66 = require(__dirname+'/configurations.js');
+var Configuration6e8bc430_9c3a_11d9_9669_0800200c9a66 = require(__dirname+'/6e8bc430-9c3a-11d9-9669-0800200c9a66.js');
 
 /**
  * Create a new Configuration that let users create sub-proxies.
@@ -10,11 +10,11 @@ var Configuration6e8bc430-9c3a-11d9-9669-0800200c9a66 = require(__dirname+'/conf
 function Configuration() { }
 
 /**
- * Create a new Configuration6e8bc430-9c3a-11d9-9669-0800200c9a66 object.
- * @return {Configuration6e8bc430-9c3a-11d9-9669-0800200c9a66}
+ * Create a new Configuration6e8bc430_9c3a_11d9_9669_0800200c9a66 object.
+ * @return {Configuration6e8bc430_9c3a_11d9_9669_0800200c9a66}
  */
-Configuration.prototype.6e8bc430-9c3a-11d9-9669-0800200c9a66 = function() {
-  return new Configuration6e8bc430-9c3a-11d9-9669-0800200c9a66();
+Configuration.prototype._6e8bc430_9c3a_11d9_9669_0800200c9a66 = function() {
+  return new Configuration6e8bc430_9c3a_11d9_9669_0800200c9a66();
 }
 
 //ORIGINAL module.exports = new Configuration();
